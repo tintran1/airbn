6b3943169be2d9600f1f8c328de4c0334f1ff58a
unknown <trannhutin.tk@gmail.com>
2022-08-02 15:52:32 +0700

    a

---
+++ javasript/javasript.js
@@ -0,0 +1,479 @@
+$(document).ready(function () {
+    $(".hinh1").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      draggable: false,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-left-long"></i></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-right-long"></i></ion-icon></button>`,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+          },
+        },
+      ],
+      
+    });
+  });
+  $(document).ready(function () {
+    $(".hinh2").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      draggable: false,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-left-long"></i></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-right-long"></i></ion-icon></button>`,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+          },
+        },
+      ],
+      
+    });
+  });
+  $(document).ready(function () {
+    $(".hinh3").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      draggable: false,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-left-long"></i></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-right-long"></i></ion-icon></button>`,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+          },
+        },
+      ],
+      
+    });
+  });
+  $(document).ready(function () {
+    $(".hinh4").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      draggable: false,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-left-long"></i></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-right-long"></i></ion-icon></button>`,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+          },
+        },
+      ],
+      
+    });
+  });
+  $(document).ready(function () {
+    $(".hinh5").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      draggable: false,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-left-long"></i></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-right-long"></i></ion-icon></button>`,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+          },
+        },
+      ],
+      
+    });
+  });
+  $(document).ready(function () {
+    $(".hinh6").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      draggable: false,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-left-long"></i></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-right-long"></i></ion-icon></button>`,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+          },
+        },
+      ],
+      
+    });
+  });
+  $(document).ready(function () {
+    $(".hinh7").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      draggable: false,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-left-long"></i></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-right-long"></i></ion-icon></button>`,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+          },
+        },
+      ],
+      
+    });
+  });
+  $(document).ready(function () {
+    $(".hinh8").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      draggable: false,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-left-long"></i></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-right-long"></i></ion-icon></button>`,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+          },
+        },
+      ],
+      
+    });
+  });
+  $(document).ready(function () {
+    $(".hinh9").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      draggable: false,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-left-long"></i></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-right-long"></i></ion-icon></button>`,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+          },
+        },
+      ],
+      
+    });
+  });
+  $(document).ready(function () {
+    $(".hinh10").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      draggable: false,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-left-long"></i></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-right-long"></i></ion-icon></button>`,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+          },
+        },
+      ],
+      
+    });
+  });
+  $(document).ready(function () {
+    $(".hinh11").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      draggable: false,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-left-long"></i></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-right-long"></i></ion-icon></button>`,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+          },
+        },
+      ],
+      
+    });
+  });
+  $(document).ready(function () {
+    $(".hinh12").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      draggable: false,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-left-long"></i></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-right-long"></i></ion-icon></button>`,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+          },
+        },
+      ],
+      
+    });
+  });
+  $(document).ready(function () {
+    $(".hinh13").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      draggable: false,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-left-long"></i></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-right-long"></i></ion-icon></button>`,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+          },
+        },
+      ],
+      
+    });
+  });
+  $(document).ready(function () {
+    $(".hinh14").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      draggable: false,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-left-long"></i></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-right-long"></i></ion-icon></button>`,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+          },
+        },
+      ],
+      
+    });
+  });
+  $(document).ready(function () {
+    $(".hinh15").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      draggable: false,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-left-long"></i></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-right-long"></i></ion-icon></button>`,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+          },
+        },
+      ],
+      
+    });
+  });
+  $(document).ready(function () {
+    $(".hinh16").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      draggable: false,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-left-long"></i></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name=<i class="fa-solid fa-arrow-right-long"></i></ion-icon></button>`,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+          },
+        },
+      ],
+      
+    });
+  });
+//   var acc = document.getElementsByClassName("slick-arrow");
+//   var i;
+
+//   for (i = 0; i < acc.length; i++) {
+//     acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var panel = this.nextElementSibling;
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+//   });
+// }
+
